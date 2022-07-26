a1c0b4a1b2987bdb463f65020eae21c60d76e9b2
Rosa Mach-Batlle <rosa.mach.batlle@gmail.com>
2022-07-26 16:52:37 +0200

    chore: csss and js

--- js/index.js
+++ js/index.js
@@ -2,4 +2,4 @@ $(document).ready(function(){
     $('#my_iframe').ready(function(){
         document.head.insertAdjacentHTML("beforeend", '<base href="https://www.chicatelegram.com/" target="_PARENT"></base>');
     });
-});​
+})
