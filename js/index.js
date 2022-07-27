68280bdd35369978928a7a05c6cb5a3ecdf9749d
Daniel Lopez Puig <daniel.lopez.puig@gmail.com>
2022-07-27 18:11:47 +0200

    fix: iframe access

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-$(document).ready(function(){
-    $('#my_iframe').ready(function(){
-        document.head.insertAdjacentHTML("beforeend", '<base href="https://www.chicatelegram.com/" target="_PARENT"></base>');
+$(document).load(function(){
+    $('#my_iframe').load(function(){
+        console.log("hey");
+        $(this).contents().find('head').html('<base href="https://www.chicatelegram.com" target="_PARENT"></base>');
     });
 })
